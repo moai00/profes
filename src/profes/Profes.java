dfecf1e89496808d7738db2ed45f44a7adea6bb0
moai00 <miquelport@gmail.com>
2016-05-10 14:16:30 +0200

    dilluns

--- src/profes/Profes.java
+++ src/profes/Profes.java
@@ -1,13 +1,11 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Aplicacion de profesores de un centro
  */
 package profes;
 
 /**
  *
- * @author usu21
+ * @author Miquel Port
  */
 public class Profes {
 
